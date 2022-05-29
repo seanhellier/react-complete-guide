c671a5044c1e59e731876db3ddd2994e199f6d71
Sean Leif Hellier <sean@newtropolis.com>
2022-05-29 15:30:39 -0400

    added date props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,17 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-	const expenseDate = new Date(2021, 2, 28);
-	const expenseTitle = "Car Insurance";
-	const expenseAmount = "288.36";
+	const month = props.date.toLocaleString("en-us", { month: "long" });
+	const day = props.date.toLocaleString("en-us", { day: "2-digit" });
+	const year = props.date.getFullYear();
 
 	return (
 		<div className="expense-item">
-			<div>{props.date.toISOString()}</div>
+			<div>
+				<div>{month}</div>
+				<div>{year}</div>
+				<div>{day}</div>
+			</div>
 			<div className="expense-item__description">
 				<h2>{props.title}</h2>
 				<div className="expense-item__price">{props.amount}</div>
