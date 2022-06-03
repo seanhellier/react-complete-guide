5a26a679a5940c940cd3253497f0325c1a099603
Sean Leif Hellier <sean@newtropolis.com>
2022-06-03 12:02:13 -0400

    generated form data capture

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
 	const [enteredTitle, setEnteredTitle] = useState("");
 	const [enteredAmount, setEnteredAmount] = useState("");
 	const [enteredDate, setEnteredDate] = useState("");
@@ -28,6 +28,10 @@ const ExpenseForm = () => {
 			date: new Date(enteredDate),
 		};
 		console.log(expenseData);
+		props.onSaveExpenseData(expenseData);
+		setEnteredTitle("");
+		setEnteredAmount("");
+		setEnteredDate("");
 	};
 
 	return (
@@ -35,7 +39,11 @@ const ExpenseForm = () => {
 			<div className="new-expense__controls">
 				<div className="new-expense__control">
 					<label>Title</label>
-					<input type="text" onChange={titleChangeHandler} />
+					<input
+						type="text"
+						value={enteredTitle}
+						onChange={titleChangeHandler}
+					/>
 				</div>
 				<div className="new-expense__control">
 					<label>Amount</label>
@@ -43,6 +51,7 @@ const ExpenseForm = () => {
 						type="number"
 						min="0.01"
 						step="0.01"
+						value={enteredAmount}
 						onChange={amountChangeHandler}
 					/>
 				</div>
@@ -52,6 +61,7 @@ const ExpenseForm = () => {
 						type="date"
 						min="2019-01-01"
 						max="2022-12-31"
+						value={enteredDate}
 						onChange={dateChangeHandler}
 					/>
 				</div>
